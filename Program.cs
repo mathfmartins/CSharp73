a4aabc636e686aa6235b8fc3e58b29b758105503
Matheus Martins <math.fmartins42@gmail.com>
2020-02-26 18:13:11 -0300

    brincando com tuples

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace CSharp73
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var tuple = TesteTuples_1.TestarTuple();
+            var pessoa = new Pessoa(); 
+
+            pessoa.Nome = tuple.nome;
+            pessoa.Idade = tuple.idade;
+            pessoa.Cpf = tuple.cpf;
+
+            Console.WriteLine(pessoa);
+        }
+
+        public class Pessoa
+        {
+            public string Nome { get; set; }
+            public string Cpf { get; set; }
+            public int Idade { get; set; }
+
+            public override string ToString()
+            {
+                return $"{Nome} | {Idade} | {Cpf}";
+            }
+        }
+    }
+}
---
+++ TesteTuples.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CSharp73
+{
+    static class TesteTuples_1
+    {
+        public static (string nome, string cpf, int idade) TestarTuple()
+        {
+            //Nome, Cpf, Idade
+            var pessoa_1 = ("Lola Matheus", "181.943.645.12", 27);
+            var pessoa_2 = ("Dante", "141.931.487.01", 39);
+
+            bool mesmaPessoa = pessoa_1 == pessoa_2 ? true : false;
+
+            if (mesmaPessoa)
+                Console.WriteLine("São a mesma pessoa");
+            else
+                Console.WriteLine("Não são a mesma pessoa");
+
+            return pessoa_1;
+        }
+    }
+}
