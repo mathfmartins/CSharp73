32c446b523c252d7918de18aa41a5bf7e3ae4afb
Matheus Martins <math.fmartins42@gmail.com>
2020-02-26 23:01:15 -0300

    usando ref com foreach

--- ReatribuicaoEmRefs.cs
+++ ReatribuicaoEmRefs.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 
 namespace CSharp73 { 
     public static class ReatribuicaoEmRefs {
@@ -6,9 +7,12 @@ namespace CSharp73 {
             var numeros = new int[] { 1, 2, 3, 42, 256, 19, 12, 01 };
             ImprimirNumeros(numeros);
             
-            ref var maiorValor = ref ObterMaiorValor(numeros);
+            ref var maiorValor = ref ObterMaiorValor_1(numeros);
             maiorValor *= 2;
+            ImprimirNumeros(numeros);
 
+            maiorValor = ref ObterMaiorValor_2(numeros);
+            maiorValor *= 4;
             ImprimirNumeros(numeros);
 
         }
@@ -16,13 +20,25 @@ namespace CSharp73 {
         public static void ImprimirNumeros(int[] numeros) => 
             Console.WriteLine(string.Join(", ", numeros));
 
-        public static ref int ObterMaiorValor(int[] numeros) { 
+        public static ref int ObterMaiorValor_1(int[] numeros) { 
             ref var maior = ref numeros[0];
-            for (int i = 0; i < numeros.Length; i++) { 
+            for (int i = 1; i < numeros.Length; i++) { 
                 if (numeros[i] > maior)
                     maior = ref numeros[i]; //reatribuindo ref \o/
             }
 
+            return ref maior;
+        }
+
+        public static ref int ObterMaiorValor_2(int[] numeros) { 
+            //Span<T> permite retornar um valor de referência para o item
+            Span<int> spanDeNumeros = new Span<int>(numeros);
+
+            ref var maior = ref numeros[0];
+            foreach (ref var item in spanDeNumeros.Slice(0)) { 
+                if (item > maior) 
+                    maior = ref item;
+            }
             return ref maior;
         }
     }
